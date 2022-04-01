4fbecee25df6bbc30e6a9489bdda944710d40def
David Song <songdasol@gmail.com>
2022-03-31 20:19:30 -0700

    completed user-can-create-entry.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+function beforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+}
+
+var previousDataJSON = localStorage.getItem('data-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('#photourl');
+var $image = document.querySelector('.image');
+var $formEntry = document.querySelector('.form-entry');
+
+$photoUrl.addEventListener('input', imageEntry);
+$formEntry.addEventListener('submit', submitForm);
+
+function imageEntry(event) {
+  var photoUrl = event.target.value;
+  $image.setAttribute('src', photoUrl);
+}
+
+function submitForm(event) {
+  var newEntryObj = {};
+  data.nextEntryId++;
+  data.entries.unshift(newEntryObj);
+  $image.setAttribute('src', '../images/placeholder-image-square.jpg');
+  $formEntry.reset();
+}
