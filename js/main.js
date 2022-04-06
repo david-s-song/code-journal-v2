6dcbdb9e82e2d2bc2a3e3eed2a199e248b0d7f05
David Song <songdasol@gmail.com>
2022-04-05 19:52:03 -0700

    added sibling to div[data-view=entries] to the HTML.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('#photourl');
+var $image = document.querySelector('.image');
+var $formEntry = document.querySelector('.form-entry');
+
+$photoUrl.addEventListener('input', imageEntry);
+$formEntry.addEventListener('submit', submitForm);
+
+function imageEntry(event) {
+  var photoUrl = event.target.value;
+  $image.setAttribute('src', photoUrl);
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  var newEntryObj = {};
+  newEntryObj.title = $formEntry.elements.title.value;
+  newEntryObj.photoUrl = $formEntry.elements.photourl.value;
+  newEntryObj.notes = $formEntry.elements.notes.value;
+  newEntryObj.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newEntryObj);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $formEntry.reset();
+}
